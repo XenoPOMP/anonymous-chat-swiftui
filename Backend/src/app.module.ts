daf4fa36ce079fafce29eb22cc6cba0b9dd70858
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-15 16:13:55 +0200

    Revert "🫗 create prisma service"
    This reverts commit 71756c065bd247500e4ee274351588332a0aedcc.

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JumpInModule } from './routes/jump-in/jump-in.module';
 import { UserModule } from './routes/user/user.module';
-import { PrismaService } from './features/prisma.service';
 
 @Module({
   imports: [JumpInModule, UserModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
