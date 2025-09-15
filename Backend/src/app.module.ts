66f54151eb80815c1a134c824707f500c64b0617
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-15 16:16:41 +0200

    🫗 create global prisma module

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JumpInModule } from './routes/jump-in/jump-in.module';
 import { UserModule } from './routes/user/user.module';
+import { PrismaModule } from './features/prisma/prisma.module';
 
 @Module({
-  imports: [JumpInModule, UserModule],
+  imports: [JumpInModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
