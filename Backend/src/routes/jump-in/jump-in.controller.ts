7a1a87033b2ee2072e1355c9156b9630af3ae4e7
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-15 15:45:38 +0200

    👨‍💻 add run scripts

--- Backend/src/routes/jump-in/jump-in.controller.ts
+++ Backend/src/routes/jump-in/jump-in.controller.ts
@@ -1,9 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { JumpInService } from './jump-in.service';
+import { Endpoint } from '../../decorators/endpoint';
 
 @Controller('jump-in')
 export class JumpInController {
   constructor(private readonly jumpInService: JumpInService) {}
 
-  async jumpUserIn() {}
+  @Endpoint('POST', '/')
+  async jumpUserIn() {
+    return 'Jumped in!';
+  }
 }
