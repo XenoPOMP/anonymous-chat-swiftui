53fd55a2b1b6d575c7947b9baca195d6f7cf039c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-15 14:46:40 +0200

    📝 initial commit

---
+++ AnonymousChat/AnonymousChat/AnonymousChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnonymousChatApp.swift
+//  AnonymousChat
+//
+//  Created by Александр on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnonymousChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnonymousChat/AnonymousChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnonymousChat
+//
+//  Created by Александр on 15.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
