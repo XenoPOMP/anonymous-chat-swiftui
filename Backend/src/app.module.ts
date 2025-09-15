fa324ba8f88b1b7ad987ea88719a2293d038068d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-15 16:10:11 +0200

    👨 create user service

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JumpInModule } from './routes/jump-in/jump-in.module';
+import { UserModule } from './routes/user/user.module';
 
 @Module({
-  imports: [JumpInModule],
+  imports: [JumpInModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
