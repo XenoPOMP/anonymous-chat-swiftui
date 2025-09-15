8226c77becf89e22346af46c9d29a6f5f6f5e10b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-15 16:34:49 +0200

    ✍️ handle user creation

--- Backend/src/routes/jump-in/jump-in.service.ts
+++ Backend/src/routes/jump-in/jump-in.service.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 import { parseCookies } from '../../utils/parse-cookies';
 import { USER_INOUT_ID_COOKIE_NAME } from '../../constants/user-inout-id';
 import { UserService } from '../user/user.service';
+import { User } from '@prisma/client';
+import { Nullable } from 'xenopomp-essentials';
 
 @Injectable()
 export class JumpInService {
@@ -12,6 +14,30 @@ export class JumpInService {
 
   async jumpUserIn(res: Response, req: Request) {
     const { inoutUserId } = parseCookies(req);
+
+    // userId is not passed through server cookies.
+    // That means that user needs to be re-created.
+    if (inoutUserId === undefined) {
+      await this.createAndAssign(res);
+      return;
+    }
+
+    // Get user from DB.
+    const oldUser: Nullable<User> = await this.userService.getById(inoutUserId);
+
+    // User does not exist. Creating new one.
+    if (!oldUser) {
+      await this.createAndAssign(res);
+      return;
+    }
+
+    // User is defined here. Adding id to cookies.
+    this.addInoutIdToResponse(res, inoutUserId);
+  }
+
+  async createAndAssign(res: Response) {
+    const { id }: User = await this.userService.create();
+    this.addInoutIdToResponse(res, id);
   }
 
   addInoutIdToResponse(res: Response, userId: string) {
--- Backend/src/routes/user/user.service.ts
+++ Backend/src/routes/user/user.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../features/prisma/prisma.service';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+  Config as NamesGeneratorConfig,
+} from 'unique-names-generator';
+import { User } from '@prisma/client';
+import { Nullable } from 'xenopomp-essentials';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create() {}
+  getRandomName(): string {
+    const config: NamesGeneratorConfig = {
+      dictionaries: [adjectives, colors, animals],
+      separator: ' ',
+      style: 'capital',
+    };
+
+    return uniqueNamesGenerator(config);
+  }
+
+  async getById(userId: string): Promise<Nullable<User>> {
+    return this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+  }
+
+  async create(): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        generatedName: this.getRandomName(),
+      },
+    });
+  }
 }
