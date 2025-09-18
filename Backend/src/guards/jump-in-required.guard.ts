e03e79925dd0f47b493b7bab679117ee4e92a3c0
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-18 20:10:52 +0200

    ✍️ write user to context by guard

--- Backend/src/guards/jump-in-required.guard.ts
+++ Backend/src/guards/jump-in-required.guard.ts
@@ -1,6 +1,7 @@
 import {
   applyDecorators,
   CanActivate,
+  createParamDecorator,
   ExecutionContext,
   Injectable,
   UseGuards,
@@ -11,29 +12,55 @@ import { UserService } from '../routes/user/user.service';
 import { Nullable } from 'xenopomp-essentials';
 import { User } from '@prisma/client';
 
+export const getUserFromCtx = (ctx: ExecutionContext): User => {
+  const req = ctx.switchToHttp().getRequest<
+    Request & {
+      user: User;
+    }
+  >();
+  return req.user;
+};
+
 @Injectable()
 export class JumpInRequiredGuard implements CanActivate {
   constructor(private readonly userService: UserService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request: Request = context.switchToHttp().getRequest<Request>();
+    const request: Request = context.switchToHttp().getRequest<
+      Request & {
+        user?: User;
+      }
+    >();
     const { inoutUserId } = parseCookies(request);
 
     // Cookie is not presented. Can not activate.
     if (!inoutUserId) {
+      // @ts-expect-error User is mapped by ourselves
+      request.user = undefined;
       return false;
     }
 
     const oldUser: Nullable<User> = await this.userService.getById(inoutUserId);
     // User with that id does not exist. Can not activate either.
     if (!oldUser) {
+      // @ts-expect-error User is mapped by ourselves
+      request.user = undefined;
       return false;
     }
 
     // User exists here. That means, that we can officially activate now.
+    // @ts-expect-error User is mapped by ourselves
+    request.user = oldUser;
     return true;
   }
 }
 
 export const JumpInRequired = () =>
   applyDecorators(UseGuards(JumpInRequiredGuard));
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof User, ctx: ExecutionContext) => {
+    const user = getUserFromCtx(ctx);
+    return data ? user[data] : user;
+  },
+);
