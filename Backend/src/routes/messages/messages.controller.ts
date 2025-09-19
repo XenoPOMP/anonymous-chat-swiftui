c02f173c093002243a9da663b9daf8790a8253de
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-19 13:02:37 +0200

    🤩 update message service

--- Backend/src/routes/messages/messages.controller.ts
+++ Backend/src/routes/messages/messages.controller.ts
@@ -1,7 +1,16 @@
 import { Controller } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { Message } from '@prisma/client';
+import { Endpoint } from '../../decorators/endpoint';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
+
+  @Endpoint('GET', '/', {
+    code: 200,
+  })
+  async getHistory(): Promise<Message[]> {
+    return this.messagesService.getAll();
+  }
 }
--- Backend/src/routes/messages/messages.service.ts
+++ Backend/src/routes/messages/messages.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../features/prisma/prisma.service';
+import { Message, Prisma } from '@prisma/client';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAll(): Promise<Message[]> {
+    return this.prisma.message.findMany();
+  }
+
+  async createOne(data: Prisma.MessageCreateArgs['data']): Promise<Message> {
+    return this.prisma.message.create({
+      data,
+    });
+  }
+}
