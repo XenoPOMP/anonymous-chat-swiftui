c36ae9f20e2754a0dd92fa140bacee556dd85658
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-19 13:39:02 +0200

    ✏️ refactor code

--- AnonymousChat/AnonymousChat/AnonymousChatApp.swift
+++ AnonymousChat/AnonymousChat/AnonymousChatApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AnonymousChatApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Main()
         }
     }
 }
--- AnonymousChat/AnonymousChat/Main.swift
+++ AnonymousChat/AnonymousChat/Main.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  Main.swift
 //  AnonymousChat
 //
 //  Created by Александр on 15.09.2025.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Main: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    Main()
 }
