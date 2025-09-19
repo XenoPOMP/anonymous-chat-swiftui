8d6a6c15edb014e99dc301b04e15cb9fdd091829
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-19 14:34:00 +0200

    🧪 create testing lib

--- AnonymousChat/AnonymousChat/Sources/App/Models/MessageModel.swift
+++ AnonymousChat/AnonymousChat/Sources/App/Models/MessageModel.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+struct ParsedMessageModel {
+    var id: String
+    var createdAt: Date
+    var generatedName: String
+    var seededColor: String
+    var textContent: String
+}
+
 struct MessageModel: Codable {
     var id: String
     var createdAt: String
