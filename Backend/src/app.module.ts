1b1d16cd9321ec6e411e67abb179dd608a3afe19
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-19 11:30:57 +0200

    🌉 init ChatGateway

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { JumpInModule } from './routes/jump-in/jump-in.module';
 import { UserModule } from './routes/user/user.module';
 import { PrismaModule } from './features/prisma/prisma.module';
+import { ChatGateway } from './realtime/chat/chat.gateway';
 
 @Module({
   imports: [JumpInModule, UserModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
